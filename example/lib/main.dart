b332aa2065219baef31f7c13541146341e72d044
rmortes <56652057+rmortes@users.noreply.github.com>
2022-06-26 15:47:21 +0000

    Example done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:form_utilities/validation.dart';
+import 'package:form_utilities/validators/num_validator.dart';
+import 'package:form_utilities/validators/str_validator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +10,106 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Form example"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Form(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Username",
+              ),
+              validator: validate([
+                ValStr().isNotEmpty(error: "The username can't be empty"),
+                ValStr()
+                    .startsWith("@", error: 'The username must start with "@"'),
+                ValStr().lengthBiggerThan(4,
+                    error: "The username must be at least 3 characters long"),
+                ValStr().lengthSmallerThan(9,
+                    error: "The username must be at most 8 characters long"),
+                ValStr().notContainsSurrogatePair(
+                    error: "Username must not contain emojis"),
+              ]),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Age",
+              ),
+              validator: validate([
+                ValInt().isValid(error: "Your age must be a whole number"),
+                ValInt().isPositive(error: "Your age must be positive"),
+                ValInt()
+                    .lt(100, error: "Everybody knows there's no one that old"),
+              ]),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+              validator: validate([
+                ValStr().isNotEmpty(error: "The email can't be empty"),
+                ValStr().isEmail(error: "The email is not valid"),
+                ValStr().lengthBiggerThan(8,
+                    error:
+                        "You don't have an email that short, don't try to fool me")
+              ]),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            // Custom hybrid validator
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "The answer to life, the universe, and everything",
+              ),
+              validator: (val) {
+                final validator = validate([
+                  ValStr().isNotEmpty(
+                      error: "There must be an answer, I just know!"),
+                  ValInt().isValid(
+                      error: "The answer must be a whole number, surely!"),
+                  (str) => int.parse(str) >= 0
+                      ? null
+                      : "You think the answer is negative? Nay mate",
+                  ValInt().neq(0,
+                      error:
+                          "The answer must not be zero, or else I have an appointment to make with my therapist"),
+                  ValInt().lt(45,
+                      error: "The answer is not THAT big, that'd be stupid!"),
+                ]);
+                final validated = validator(val);
+                if (validated != null) {
+                  return validated;
+                }
+                if (int.parse(val!) == 42) {
+                  return "Ah, so you've read The Hitchhiker's Guide to the Galaxy, right?";
+                }
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
